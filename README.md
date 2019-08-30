2f03dc3a7823fa177eaf84993ff303dbc6fb2896
valdenbe <pirghaye.f@gmail.com>
2019-08-30 09:25:17 +0200

    Add ReadMe and some comments

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
-This is a simple program that allows you to check vacation days in a country.
-To use it, type 'holidates CountryName'  in the terminal.
+<h1 align="center">Welcome to Holidates 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.5-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/ValdenBE/holidates#readme">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" target="_blank" />
+  </a>
+  <a href="https://github.com/ValdenBE/holidates/graphs/commit-activity">
+    <img alt="Maintenance" src="https://img.shields.io/badge/Maintained%3F-yes-green.svg" target="_blank" />
+  </a>
+  <a href="https://github.com/ValdenBE/holidates/blob/master/LICENSE">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" target="_blank" />
+  </a>
+  <a href="https://twitter.com/V_A_L_D_E_N">
+    <img alt="Twitter" src="https://img.shields.io/twitter/follow/V_A_L_D_E_N.svg?style=social" target="_blank" />
+  </a>
+</p>
 
+> A script for checking holidays in country's.
+
+### 🏠 [Homepage](https://github.com/ValdenBE/holidates#readme)
+
+## Install
+
+```sh
+npm i @valdenbe/holidates
+```
+
+## Usage
+
+```sh
+npx @valdenbe/holidates
+```
+
+## Author
+
+👤 **Valden**
+
+* Twitter: [@V_A_L_D_E_N](https://twitter.com/V_A_L_D_E_N)
+* Github: [@ValdenBE](https://github.com/ValdenBE)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/ValdenBE/holidates/issues).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+<a href="https://www.patreon.com/valdebe">
+  <img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png" width="160">
+</a>
+
+## 📝 License
+
+Copyright © 2019 [Valden](https://github.com/ValdenBE).<br />
+This project is [MIT](https://github.com/ValdenBE/holidates/blob/master/LICENSE) licensed.
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 #!/usr/bin/env node
+// CountryList package :
 const countryList = require('country-list');
-//
+// Api request :
 const axios = require('axios');
-//
+// Text colors :
 const chalk = require('chalk');
-
+// Clear terminal package :
 const clear = require('clear');
-//
-const api =  'https://date.nager.at/api/v2/PublicHolidays/';
-//
-var date = new Date();
-//
-var year = date.getFullYear();
-//
-const args = process.argv;
-//console.log(args)
-var country = args[2];
-//console.log(country)
-const { getCode } = countryList;
-//
-const countryCode = getCode(country);
-//console.log(countryCode);
-var figlet = require('figlet');
-clear()
+// Logo :
+const figlet = require('figlet');
+// Return to line in terminal :
 const newline = '\n';
+// Api :
+const api =  'https://date.nager.at/api/v2/PublicHolidays/';
+// Date of today :
+var date = new Date();
+// Get year :
+var year = date.getFullYear();
+// Get argument in terminal :
+const args = process.argv;
+// Select argument :
+var country = args[2];
+// Create the " Check code" from countryList :
+const { getCode } = countryList;
+// Check for the countryCode :
+const countryCode = getCode(country);
+//  Clear the terminal at start :
+clear()
+// Place the " Logo " :
 figlet('HOLIDATES', function(err, data) {
     if (err) {
         console.log('Something went wrong...');
@@ -32,11 +36,12 @@ figlet('HOLIDATES', function(err, data) {
     }
     console.log(data)
 });
+// Main script : ( Place a title, consult the API with the argument and log response (with layout))
     const holidates = async () => {
         try {
             const res = await axios.get(`${api}${year}/${countryCode}`)
             var data = res.data
-            console.log(chalk.red.underline(newline + newline +'Here is the list of holidays in '+ country + ':' + newline));
+            console.log(chalk.red.underline(newline + newline +'Here is the list of holidays in '+ country + ':' + newline)); 
             data.forEach(element =>
                 {
                     console.log(chalk.red('Date : ') + element.date + '   ' + (chalk.red( 'Name : ')) + element.name);
